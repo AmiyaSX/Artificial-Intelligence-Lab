e24f83fd70d8f15770c1a35a652555e715736733
AmiyaSX <2668590277@qq.com>
2024-11-18 16:50:22 +0100

    Add minmax algorithm

--- minimax_assignment/player.py
+++ minimax_assignment/player.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import math
 
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
@@ -50,6 +51,47 @@ class PlayerControllerMinimax(PlayerController):
             # Execute next action
             self.sender({"action": best_move, "search_time": None})
 
+    def heuristic_function(self, current_node):
+        max_score = current_node.state.player_scores[0]
+        min_score = current_node.state.player_scores[1]
+        (max_hook_x, max_hook_y) = current_node.state.hook_positions[0]
+        (min_hook_x, min_hook_y) = current_node.state.hook_positions[1]
+        uncaught_fishes = current_node.state.fish_positions
+        fish_scores = current_node.state.fish_scores
+        
+        player_advantage = 0
+        for idx, (x, y) in uncaught_fishes.items():
+            fish_score = fish_scores[idx]
+            distance = abs(max_hook_x - x) + abs(max_hook_y - y)
+            if distance > 0: player_advantage += distance/fish_score
+            else: player_advantage += fish_score
+        
+        opponent_advantage = 0
+        for idx, (x, y) in uncaught_fishes.items():
+            fish_score = fish_scores[idx]
+            distance = abs(min_hook_x - x) + abs(min_hook_y - y)
+            if distance > 0: opponent_advantage += distance/fish_score
+            else: opponent_advantage += fish_score
+        
+        print(player_advantage-opponent_advantage)
+        return player_advantage - opponent_advantage
+            
+            
+        
+    def minmax(self, current_node, depth, a, b):
+        current_node.compute_and_get_children()
+        if depth == 0:
+            return self.heuristic_function(current_node=current_node) #static value of this posistion
+        maxEval = -math.inf
+        
+        for node in current_node.children:
+            eval = self.minmax(current_node=node, depth=depth-1, a=b, b=b)
+            maxEval = max(maxEval, eval)
+            a = max(a, eval)
+            if b <= a:
+                break
+        return maxEval
+        
     def search_best_next_move(self, initial_tree_node):
         """
         Use minimax (and extensions) to find best possible next move for player 0 (green boat)
@@ -64,8 +106,17 @@ class PlayerControllerMinimax(PlayerController):
 
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
+        initial_tree_node.children = initial_tree_node.compute_and_get_children()
+        maxVal = -math.inf
+        nextNode = initial_tree_node.children[0]
+        # print(initial_tree_node.state.fish_positions)
+        # print(initial_tree_node.state.fish_scores)
+        for node in initial_tree_node.children:
+            val = self.minmax(current_node=initial_tree_node, depth=3, a=-math.inf, b=math.inf)
+            if(maxVal < val):
+                maxVal = val
+                nextNode = node
 
-        initial_tree_node.children = self.compute_and_get_children();
-        
-        random_move = random.randrange(5)
+        random_move = nextNode.move
+        print(nextNode.move)
         return ACTION_TO_STR[random_move]
