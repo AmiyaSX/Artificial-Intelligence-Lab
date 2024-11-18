224ec9f0f527370baaed1601a7a68140e4c989c6
AmiyaSX <2668590277@qq.com>
2024-11-18 17:13:59 +0100

    Fix iteration

--- minimax_assignment/player.py
+++ minimax_assignment/player.py
@@ -52,28 +52,22 @@ class PlayerControllerMinimax(PlayerController):
             self.sender({"action": best_move, "search_time": None})
 
     def heuristic_function(self, current_node):
-        max_score = current_node.state.player_scores[0]
-        min_score = current_node.state.player_scores[1]
         (max_hook_x, max_hook_y) = current_node.state.hook_positions[0]
         (min_hook_x, min_hook_y) = current_node.state.hook_positions[1]
         uncaught_fishes = current_node.state.fish_positions
         fish_scores = current_node.state.fish_scores
         
         player_advantage = 0
-        for idx, (x, y) in uncaught_fishes.items():
-            fish_score = fish_scores[idx]
-            distance = abs(max_hook_x - x) + abs(max_hook_y - y)
-            if distance > 0: player_advantage += distance/fish_score
-            else: player_advantage += fish_score
-        
         opponent_advantage = 0
         for idx, (x, y) in uncaught_fishes.items():
             fish_score = fish_scores[idx]
-            distance = abs(min_hook_x - x) + abs(min_hook_y - y)
-            if distance > 0: opponent_advantage += distance/fish_score
+            p_distance = abs(max_hook_x - x) + abs(max_hook_y - y)
+            if p_distance > 0: player_advantage += fish_score/p_distance
+            else: player_advantage += fish_score
+            o_distance = abs(min_hook_x - x) + abs(min_hook_y - y)
+            if o_distance > 0: opponent_advantage += fish_score/o_distance
             else: opponent_advantage += fish_score
         
-        print(player_advantage-opponent_advantage)
         return player_advantage - opponent_advantage
             
             
@@ -82,12 +76,12 @@ class PlayerControllerMinimax(PlayerController):
         current_node.compute_and_get_children()
         if depth == 0:
             return self.heuristic_function(current_node=current_node) #static value of this posistion
-        maxEval = -math.inf
-        
+        maxEval = -9999
         for node in current_node.children:
-            eval = self.minmax(current_node=node, depth=depth-1, a=b, b=b)
-            maxEval = max(maxEval, eval)
-            a = max(a, eval)
+            _eval = self.minmax(current_node=node, depth=depth-1, a=a, b=b)
+            maxEval = max(maxEval, _eval)
+            b = min(b, _eval)
+            a = max(a, _eval)
             if b <= a:
                 break
         return maxEval
@@ -107,16 +101,13 @@ class PlayerControllerMinimax(PlayerController):
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
         initial_tree_node.children = initial_tree_node.compute_and_get_children()
-        maxVal = -math.inf
+        maxVal = -9999
         nextNode = initial_tree_node.children[0]
-        # print(initial_tree_node.state.fish_positions)
-        # print(initial_tree_node.state.fish_scores)
         for node in initial_tree_node.children:
-            val = self.minmax(current_node=initial_tree_node, depth=3, a=-math.inf, b=math.inf)
+            val = self.minmax(current_node=node, depth=7, a=-9999, b=9999)
             if(maxVal < val):
                 maxVal = val
                 nextNode = node
 
-        random_move = nextNode.move
-        print(nextNode.move)
-        return ACTION_TO_STR[random_move]
+        move = nextNode.move
+        return ACTION_TO_STR[move]
