4def743eb9daf74a3176c027b399b34cba27ebfb
AmiyaSX <2668590277@qq.com>
2024-11-19 14:34:23 +0100

    modify

--- minimax_assignment/player.py
+++ minimax_assignment/player.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import random
 import math
+from functools import lru_cache
 
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
@@ -50,7 +51,8 @@ class PlayerControllerMinimax(PlayerController):
 
             # Execute next action
             self.sender({"action": best_move, "search_time": None})
-
+    
+    @lru_cache(None)  # Cache heuristic calculations
     def heuristic_function(self, current_node):
         (max_hook_x, max_hook_y) = current_node.state.hook_positions[0]
         (min_hook_x, min_hook_y) = current_node.state.hook_positions[1]
@@ -61,39 +63,41 @@ class PlayerControllerMinimax(PlayerController):
         opponent_advantage = 0
         for idx, (x, y) in uncaught_fishes.items():
             fish_score = fish_scores[idx]
-            p_distance = abs(max_hook_x - x) + abs(max_hook_y - y)
-            if p_distance > 0: player_advantage += fish_score/p_distance
-            else: player_advantage += fish_score
-            o_distance = abs(min_hook_x - x) + abs(min_hook_y - y)
-            if o_distance > 0: opponent_advantage += fish_score/o_distance
-            else: opponent_advantage += fish_score
-        
+            p_distance = (max_hook_x - x) ** 2 + (max_hook_y - y) ** 2 
+            o_distance = (min_hook_x - x) ** 2 + (min_hook_y - y) ** 2
+            
+            player_advantage += fish_score / (p_distance + 1)
+            opponent_advantage += fish_score / (o_distance + 1)
+
         return player_advantage - opponent_advantage
             
             
         
     def minmax(self, current_node, depth, a, b, maximizing_player=True):
         current_node.compute_and_get_children()
-        if depth == 0:
+        if depth == 0 or not current_node.children:
             return self.heuristic_function(current_node=current_node) #static value of this posistion
+        
+        current_node.children.sort(key=lambda node: self.heuristic_function(node), reverse=maximizing_player)
+        
         if maximizing_player:
-            maxEval = -9999
+            max_eval = -9999
             for node in current_node.children:
                 _eval = self.minmax(current_node=node, depth=depth-1, a=a, b=b, maximizing_player=False)
-                maxEval = max(maxEval, _eval)
+                max_eval = max(max_eval, _eval)
                 a = max(a, _eval)
                 if b <= a:
                     break
-            return maxEval
+            return max_eval
         else:
-            minEval = 9999
+            min_eval = 9999
             for node in current_node.children:
                 _eval = self.minmax(current_node=node, depth=depth-1, a=a, b=b, maximizing_player=True)
-                minEval = min(minEval, _eval)
+                min_eval = min(min_eval, _eval)
                 b = min(b, _eval)
                 if b <= a:
                     break
-            return minEval
+            return min_eval
         
     def search_best_next_move(self, initial_tree_node):
         """
@@ -110,14 +114,13 @@ class PlayerControllerMinimax(PlayerController):
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
         initial_tree_node.compute_and_get_children()
-        initial_tree_node.children.sort(key=lambda node: self.heuristic_function(node), reverse=True)
-        maxVal = -9999
-        nextNode = initial_tree_node.children[0]
+        max_val = -9999
+        next_node = initial_tree_node.children[0]
         for node in initial_tree_node.children:
             val = self.minmax(current_node=node, depth=3, a=-9999, b=9999, maximizing_player=True)
-            if(maxVal < val):
-                maxVal = val
-                nextNode = node
+            if(max_val < val):
+                max_val = val
+                next_node = node
 
-        move = nextNode.move
-        return ACTION_TO_STR[move]
+        move = next_node.move
+        return ACTION_TO_STR[move] if next_node else 0
