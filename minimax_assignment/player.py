7b2179d5ca10bd99322969900fb3d987ab4220bf
AmiyaSX <2668590277@qq.com>
2024-11-22 00:08:37 +0100

    finish optimize

--- minimax_assignment/player.py
+++ minimax_assignment/player.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import random
 import math
+import time
 from functools import lru_cache
 
 from fishing_game_core.game_tree import Node
@@ -52,52 +53,78 @@ class PlayerControllerMinimax(PlayerController):
             # Execute next action
             self.sender({"action": best_move, "search_time": None})
     
+    def isInstantWin(self, hook_positions, fish_positions):
+        return hook_positions == fish_positions
+    
+    def state_to_str(self, state):
+        return str(state.get_hook_positions()) + str(state.get_fish_positions())
+    
     @lru_cache(None)  # Cache heuristic calculations
     def heuristic_function(self, current_node):
-        (max_hook_x, max_hook_y) = current_node.state.hook_positions[0]
-        (min_hook_x, min_hook_y) = current_node.state.hook_positions[1]
-        uncaught_fishes = current_node.state.fish_positions
+        p_score = current_node.state.player_scores[0]
+        o_score = current_node.state.player_scores[1]
+        
+        (p_hook_x, p_hook_y) = current_node.state.hook_positions[0]
+        (o_hook_x, o_hook_y) = current_node.state.hook_positions[1]
+        
+        fish_positions = current_node.state.fish_positions
         fish_scores = current_node.state.fish_scores
         
-        player_advantage = 0
-        opponent_advantage = 0
-        for idx, (x, y) in uncaught_fishes.items():
-            fish_score = fish_scores[idx]
-            p_distance = (max_hook_x - x) ** 2 + (max_hook_y - y) ** 2 
-            o_distance = (min_hook_x - x) ** 2 + (min_hook_y - y) ** 2
+        value = 0
+        
+        for idx, (fish_x, fish_y) in fish_positions.items():
+            fish_value = fish_scores[idx]
+            p_distance = min(abs(p_hook_x - fish_x), 20 - abs(p_hook_x - fish_x)) + abs(p_hook_y - fish_y)
+            # o_distance = min(abs(o_hook_x - fish_x), 20 - abs(o_hook_x - fish_x)) + abs(o_hook_y - fish_y)
             
-            player_advantage += fish_score / (p_distance + 1)
-            opponent_advantage += fish_score / (o_distance + 1)
+            # Instant win check
+            if fish_value > 0 and self.isInstantWin((p_hook_x, p_hook_y), (fish_x, fish_y)):
+                return math.inf
+            if fish_value > 0 and self.isInstantWin((o_hook_x, o_hook_y), (fish_x, fish_y)):
+                value -= math.inf
+            
+            value = max(fish_value * math.exp(-p_distance), value)
+        
+        s_diff = 3
+        
+        return s_diff * (p_score - o_score) + value
 
-        return player_advantage - opponent_advantage
-            
             
         
-    def minmax(self, current_node, depth, a, b, maximizing_player=True):
-        current_node.compute_and_get_children()
-        if depth == 0 or not current_node.children:
-            return self.heuristic_function(current_node=current_node) #static value of this posistion
+    def minmax(self, current_node, depth, a, b, visited_nodes, maximizing_player=True, start_time=0):
+        if time.time() - start_time > 0.05:
+            raise TimeoutError
         
-        current_node.children.sort(key=lambda node: self.heuristic_function(node), reverse=maximizing_player)
+        MAX = math.inf
+        v = 0
+        k = self.state_to_str(current_node.state)
+        if k in visited_nodes and visited_nodes[k][0] >= depth:
+            return visited_nodes[k][1]
         
-        if maximizing_player:
-            max_eval = -9999
-            for node in current_node.children:
-                _eval = self.minmax(current_node=node, depth=depth-1, a=a, b=b, maximizing_player=False)
-                max_eval = max(max_eval, _eval)
+        nodes = current_node.compute_and_get_children()
+        nodes.sort(key=lambda node: self.heuristic_function(node), reverse=True)
+        
+        if depth == 0 or len(nodes) == 0:
+            v = self.heuristic_function(current_node) #static value of this posistion
+        elif maximizing_player:
+            max_eval = -MAX
+            for node in nodes:
+                _eval = self.minmax(node, depth-1, a, b, visited_nodes, False, start_time)
+                v = max(max_eval, _eval)
                 a = max(a, _eval)
                 if b <= a:
                     break
-            return max_eval
         else:
-            min_eval = 9999
-            for node in current_node.children:
-                _eval = self.minmax(current_node=node, depth=depth-1, a=a, b=b, maximizing_player=True)
-                min_eval = min(min_eval, _eval)
+            min_eval = MAX
+            for node in nodes:
+                _eval = self.minmax(node, depth-1, a, b, visited_nodes, True, start_time)
+                v = min(min_eval, _eval)
                 b = min(b, _eval)
                 if b <= a:
                     break
-            return min_eval
+        
+        visited_nodes.update({k:[depth,v]})
+        return v
         
     def search_best_next_move(self, initial_tree_node):
         """
@@ -113,14 +140,26 @@ class PlayerControllerMinimax(PlayerController):
 
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
+        start_time = time.time()
         initial_tree_node.compute_and_get_children()
-        max_val = -9999
-        next_node = initial_tree_node.children[0]
-        for node in initial_tree_node.children:
-            val = self.minmax(current_node=node, depth=3, a=-9999, b=9999, maximizing_player=True)
-            if(max_val < val):
-                max_val = val
-                next_node = node
+        max_val = -math.inf
+        best_move = 0
+        depth = 0
+        visited_nodes = dict()
+        
+        while True:
+            try:
+                for node in initial_tree_node.children:
+                    val = self.minmax(node, depth, -math.inf, math.inf, visited_nodes, False, start_time)
+                    if(val == math.inf): # Marked as a instant win
+                        best_move = node.move
+                        return ACTION_TO_STR[best_move]
+                    if(max_val < val):
+                        max_val = val
+                        best_move = node.move
+                depth = depth + 1
+            except:
+                break
 
-        move = next_node.move
-        return ACTION_TO_STR[move] if next_node else 0
+        
+        return ACTION_TO_STR[best_move]
